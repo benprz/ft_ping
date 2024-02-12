50cb781242df0583062adf9daae675a306efa361
Benjamin Perez <bperez@z4r2p7.42lyon.fr>
2024-02-12 17:01:19 +0100

    .

--- Makefile
+++ Makefile
@@ -1,7 +1,43 @@
-all:
-	gcc main.c -o ft_ping -Wall -Wextra -Werror -pedantic -std=c99
+#MAKEFLAGS += --silent
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -Wpedantic -g
+CDEBUGFLAGS = -g
+INC_DIR = includes/
+
+EXE = ft_ping
+
+SRC_DIR = ./
+SRC =	main.c
+
+LIBRARY_DIR = ./
+
+LIBFT_DIR = $(LIBRARY_DIR)libft/
+LIBFT_INC_DIR = $(LIBFT_DIR)includes/
+LIBFT_INC = -I $(LIBFT_INC_DIR) $(LIBFT_DIR)libft.a
+
+OBJ = $(SRC:%.c=%.o)
+
+.PHONY : all clean fclean re $(LIBFT_DIR) $(EXE)
+
+all: $(LIBFT_DIR) $(EXE)
+
+$(EXE): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ_DIR)main.o $(LIBFT_INC) -o $(EXE)
+
+$(LIBFT_DIR):
+	$(MAKE) -C $(LIBFT_DIR)
+
+%.o: $(SRC_DIR)%.c $(addprefix $(INC_DIR),$(INC))
+	$(CC) $(CFLAGS) -c $< -o $@ -I $(LIBFT_INC_DIR) -I $(INC_DIR)
 
 clean:
-	rm -f ft_ping
+	/bin/rm -rf *.o
+	$(MAKE) -C $(LIBFT_DIR) fclean
 
-re: clean all
+fclean: clean
+	/bin/rm -f $(EXE)
+
+re: 
+	$(MAKE) fclean
+	$(MAKE) all
--- main.c
+++ main.c
@@ -18,3 +18,79 @@
 ◦printf and its family.
 ◦Your libft functions.
 */
+
+#include "libft.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+//getaddrinfo
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+//inet_pton
+#include <arpa/inet.h>
+
+struct s_ping
+{
+    int verbose;
+    char *host;
+};
+
+struct s_ping g_ping = {0, NULL};
+
+int parse_host(char *host)
+{
+    // check if host is a valid ipv4 address
+    // if (inet_pton(AF_INET, host, NULL) == 0)
+    //     return (1);
+
+    // g_ping.host = host;
+    
+    return 0;
+}
+
+int parse_args(char **argv)
+{
+    for (int i = 1; argv[i]; i++)
+    {
+        if (ft_strcmp(argv[i], "-v") == 0)
+        {
+            printf("Verbose mode\n");
+            g_ping.verbose = 1;
+        }
+        else if (parse_host(argv[i]) == 1)
+        {
+            printf("Invalid host\n");
+            return (1);
+        }
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2 || argc > 3)
+    {
+        printf("Usage: ping [-v] destination\n");
+        return (1);
+    }
+
+    if (getuid() != 0)
+    {
+        printf("You must be root to use ping\n");
+        return (1);
+    }
+
+    if (parse_args(argv) == 1)
+    {
+        return (1);
+    }
+
+    // printf("Host: %s\n", g_ping.host);
+    printf("Verbose: %d\n", g_ping.verbose);
+
+    return (0);
+}
